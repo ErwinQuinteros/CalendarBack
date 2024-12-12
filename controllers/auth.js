46bc16bf2c79b5cb92405271b302eff299d74e78
Erwin Quinteros <erwinquinterosvillarroel@gmail.com>
2024-12-12 20:39:10 -0300

    user validate

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,13 +2,26 @@ const { response } = require("express");
 const User = require("../models/user");
 
 const crearUsuario = async (req, res = response) => {
+    
+    const { email, password } = req.body;
+    
     try {
-        const user = new User(req.body);
+
+        let user = await User.findOne({ email })
+        if( user ) {
+            res.status(201).json({
+                ok: false,
+                msg: "The user exists with this email",
+            });
+        }
+
+        user = new User(req.body);
         await user.save();
 
         res.status(201).json({
             ok: true,
-            msg: "registro",
+            uid: user.id,
+            name: user.name
         });
     } catch (error) {
         res.status(500).json({
@@ -20,7 +33,7 @@ const crearUsuario = async (req, res = response) => {
 
 const loginUsuario = (req, res = response) => {
     const { email, password } = req.body;
-
+    
     res.status(201).json({
         ok: true,
         msg: "login",
